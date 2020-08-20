57a726e3e443d79e9255829ac93048e5498acae8
aoshiro000 <aoshiro3@huskers.unl.edu>
2020-08-20 16:54:06 +0000

    add my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,19 @@
 /**
- *  Author: Atsuki Oshiro
+ * Author: Atsuki Oshiro
+ *         cbourke@cse.unl.edu
  * Date: 2020/08/17
  *
  * A simple hello world program in C
  *
  */
- #include <stdlib.h>
- #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
- int main(int argc, char **argv) {
+int main(int argc, char **argv) {
 
-     printf("Atsuki Oshiro");
+  printf("Atsuki Oshiro!\n");
+  printf("Computer science");
 
-     return 0;
- }
+
+  return 0;
+}
