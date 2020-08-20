bad29e948a1a9350826949e8d231eb787ea23e77
aoshiro000 <aoshiro3@huskers.unl.edu>
2020-08-20 16:20:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *  Author: Atsuki Oshiro
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Atsuki Oshiro");
+
+     return 0;
+ }
